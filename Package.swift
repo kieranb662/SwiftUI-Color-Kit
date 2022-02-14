83d7c8f9190077ec9c5a7b95ed3bb7125ffda69e
Jonathan Cole <joncole04412@gmail.com>
2022-02-14 16:30:50 -0500

    Fix incorrect reference to old Shapes package

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ColorKit",
-            dependencies: ["CGExtender","Shapes","Sliders"]),
+            dependencies: ["CGExtender","SwiftUI-Shapes","Sliders"]),
         .testTarget(
             name: "ColorKitTests",
             dependencies: ["ColorKit"]),
