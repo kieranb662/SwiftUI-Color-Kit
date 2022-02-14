f1d651f72a4e0568ad05cda1231ae678e8f35a89
Jonathan Cole <joncole04412@gmail.com>
2022-02-14 16:47:08 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ColorKit",
-            dependencies: ["CGExtender","SwiftUI-Shapes","Sliders-SwiftUI"]),
+            dependencies: ["CGExtender","Shapes","Sliders-SwiftUI"]),
         .testTarget(
             name: "ColorKitTests",
             dependencies: ["ColorKit"]),
